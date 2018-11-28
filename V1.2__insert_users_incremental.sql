97d7a3f25f7d3b9a9f40b5d1ae68e6c1a86ce501
Jonathan C <43475199+Jchunl@users.noreply.github.com>
2018-11-28 15:00:22 -0500

    Add files via upload

---
+++ V1.2__insert_users_incremental.sql
@@ -0,0 +1,14 @@
+INSERT INTO [dbo].[UserAccount]
+           ([UserID]
+           ,[FirstName]
+           ,[LastName]
+           ,[Email]
+           ,[Country]
+           ,[Company])
+     VALUES
+           (101
+           ,'Stan'
+           ,'Marsh'
+           ,'stanmarsh@email.com'
+           ,'Thailand'
+           ,'Southie Industry');
